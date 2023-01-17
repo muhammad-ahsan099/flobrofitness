301765dde0d4334cef5efc2950984c7b444b1296
husnain917 <honeyansari917@gmail.com>
2023-01-18 02:55:55 +0500

    start design and make setup

---
+++ src/assests/colors/Colors.js
@@ -0,0 +1,6 @@
+export const COLORS = {
+    white: '#fff',
+    blue: '#184163',
+    black: '#000000',
+    lightBlue: '#337ab7'
+}
---
+++ src/assests/fonts/Fonts.js
@@ -0,0 +1,3 @@
+export const FONTS = {
+    fontFamily: `'Open Sans', helvetica, arial, sans-serif;`,
+}
